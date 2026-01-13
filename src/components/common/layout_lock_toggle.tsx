f0774616bbbe5f52f38bd47ee495541a5ce70261
cryptobanter-matt <matt.kriel@cryptobanter.com>
2026-01-13 14:20:36 +0200

    shake animation fix

--- src/components/common/layout_lock_toggle.tsx
+++ src/components/common/layout_lock_toggle.tsx
@@ -1,17 +1,20 @@
-import { useEffect, useState } from 'preact/hooks';
+import { useEffect, useState, useRef } from 'preact/hooks';
 import { layout_locked, toggle_layout_lock, lock_shake } from '../../stores/layout_lock_store';
 
 export function LayoutLockToggle() {
     const is_locked = layout_locked.value;
     const shake_trigger = lock_shake.value;
     const [is_shaking, set_is_shaking] = useState(false);
+    const prev_shake_trigger = useRef(shake_trigger);
 
     useEffect(() => {
-        if (shake_trigger > 0 && is_locked) {
+        if (shake_trigger > prev_shake_trigger.current && is_locked) {
             set_is_shaking(true);
             const timeout = setTimeout(() => set_is_shaking(false), 500);
+            prev_shake_trigger.current = shake_trigger;
             return () => clearTimeout(timeout);
         }
+        prev_shake_trigger.current = shake_trigger;
     }, [shake_trigger, is_locked]);
 
     return (
